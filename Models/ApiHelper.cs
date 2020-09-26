1485a3fa5a35f211f1f1df7e964c1daf60a6a52b
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-25 18:18:41 -0700

    write static methods for the ApiHelper to DRY code

--- Models/ApiHelper.cs
+++ Models/ApiHelper.cs
@@ -1,9 +1,11 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System.Threading.Tasks;
 
 namespace NewsGator.Models
 {
-  class ApiHelper
+  public static class ApiHelper
   {
     public static async Task<string> ApiCall(string source, string endpoint)
     {
@@ -11,6 +13,29 @@ namespace NewsGator.Models
       RestRequest request = new RestRequest(endpoint, Method.GET);
       IRestResponse response = await client.ExecuteTaskAsync(request);
       return response.Content;
+    }
+
+    public static JArray Deserialize(string jsonString, string arrayName)
+    {
+      JObject jsonObject = JsonConvert.DeserializeObject<JObject>(jsonString);
+      JArray jsonArray = JsonConvert.DeserializeObject<JArray>(jsonObject.GetValue($"{arrayName}").ToString());
+      return jsonArray;
+    }
+
+    public static string[] ValueGet(JToken jsonToken, string[] values)
+    {
+      JObject jsonObj = jsonToken as JObject;
+      string[] returnArray = new string[values.Length];
+      for (int i = 0; i < values.Length; i++) {
+        returnArray[i] = jsonObj.GetValue(values[i]).ToString();
+      }
+      return returnArray;
+    }
+
+    public static string ValueGet(JToken jsonToken, string childName, string valueName)
+    {
+      JObject jsonObj = jsonToken as JObject;
+      return JsonConvert.DeserializeObject<JObject>(jsonObj.GetValue(childName).ToString()).GetValue(valueName).ToString();
     }
   }
 }
--- Models/NewsApiArticle.cs
+++ Models/NewsApiArticle.cs
@@ -1,5 +1,3 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,22 +5,21 @@ namespace NewsGator.Models
 {
   public static class NewsApiArticle
   {
+    public static string Target { get; } = "https://newsapi.org/v2/";
+
     public static List<Article> GetTopHeadlines()
     {
-      Task<string> apiCallTask = ApiHelper.ApiCall("https://newsapi.org/v2/", $"top-headlines?sources=bbc-news,associated-press,reuters,bloomberg&apiKey={EnvironmentalVariables.NewsApiKey}");
-      string result = apiCallTask.Result;
-      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-      JArray jsonArticles = JsonConvert.DeserializeObject<JArray>(jsonResponse["articles"].ToString());
-
+      string endpoint = $"top-headlines?sources=bbc-news,associated-press,reuters,bloomberg&apiKey={EnvironmentalVariables.NewsApiKey}";
+      Task<string> apiCallTask = ApiHelper.ApiCall(Target, endpoint);
+      var jsonArticles = ApiHelper.Deserialize(apiCallTask.Result, "articles");
+      
       List<Article> articleList = new List<Article>();
-      foreach(JObject jArticle in jsonArticles)
+      foreach(var jArticle in jsonArticles)
       {
-        string name = JsonConvert.DeserializeObject<JObject>(jArticle.GetValue("source").ToString()).GetValue("name").ToString();
-        string author = jArticle.GetValue("author").ToString();
-        string title = jArticle.GetValue("title").ToString();
-        string description = jArticle.GetValue("description").ToString();
-        string url = jArticle.GetValue("url").ToString();
-        articleList.Add(new Article(name, author, title, description, url));
+        string name = ApiHelper.ValueGet(jArticle, "source", "name");
+        string[] vals = { "author", "title", "description", "url" };
+        string[] valArr = ApiHelper.ValueGet(jArticle, vals);
+        articleList.Add(new Article(name, valArr[0], valArr[1], valArr[2], valArr[3]));
       }
       return articleList;
     }
