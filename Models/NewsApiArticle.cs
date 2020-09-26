00a83f9527650d3a9a68bb202260cbb1bb9cc341
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-25 17:14:53 -0700

    convert NewsApiArticle to static

--- Models/NewsApiArticle.cs
+++ Models/NewsApiArticle.cs
@@ -5,14 +5,8 @@ using System.Threading.Tasks;
 
 namespace NewsGator.Models
 {
-  public class NewsApiArticle
+  public static class NewsApiArticle
   {
-    public JObject Source { get; set; }
-    public string Author { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Url { get; set; }
-
     public static List<Article> GetTopHeadlines()
     {
       Task<string> apiCallTask = ApiHelper.ApiCall("https://newsapi.org/v2/", $"top-headlines?sources=bbc-news,associated-press,reuters,bloomberg&apiKey={EnvironmentalVariables.NewsApiKey}");
@@ -30,12 +24,6 @@ namespace NewsGator.Models
         string url = jArticle.GetValue("url").ToString();
         articleList.Add(new Article(name, author, title, description, url));
       }
-      // List<NewsApiArticle> newsApiList = JsonConvert.DeserializeObject<List<NewsApiArticle>>(jsonArticles.ToString());
-      // foreach(NewsApiArticle article in newsApiList)
-      // {
-      //   string Name = JsonConvert.DeserializeObject<string>(article.Source.ToString());
-      //   articleList.Add(new Article(Name, article.Author, article.Title, article.Description, article.Url));
-      // }
       return articleList;
     }
   }
